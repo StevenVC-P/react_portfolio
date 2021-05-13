acf33bdb8c3c34a9461f166e806d20fccd6b6a21
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-05-13 09:06:40 -0500

    added container, row, and col components, and laid out about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,30 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import Container from "../components/Container";
+import my_pic from "../images/Profesional Rounded.png";
 
 function About () {
     return (
         <div>
-         Stuff   
-
-
-
+            <Container style={{ marginTop:20}}>
+                <Row>
+                    <Col size="md-12">
+                    <h5>About Me</h5>
+                    </Col>
+                </Row>
+                <Row className="d-flex align-items-center">
+                    <Col size="sm-12 md-6 lg-4 xl-3">
+                        <img src= {my_pic} width="300" height="300" alt="Picture of me" />
+                    </Col>
+                    <Col size="sm-12 md-6 lg-7 xl-8">
+                            <p>I am a Full Stack Developer after a career swtich from auditing.  While I still enjoy thinking and planning around risks, I've found a creative spark in building. I am currently enrolled at the University of Minnesota Bootcamp.
+                                From Coon Rapids, I am an avid gamer with popular titles World of Warcraft and Minecraft and have begun learning how I can use programming skill to better both my and friends experince in these games.
+                            </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
