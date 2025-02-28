30582232b1f5e1845c9ecca4252ab8e8b077a47a
StevenVC-P <steven.vancourt@gmail.com>
2025-02-27 22:03:29 -0600

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const root = document.getElementById("root");
-const appRoot = createRoot(root);
-appRoot.render(<App />);
